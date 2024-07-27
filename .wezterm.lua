dec291dfdb99583167ac157df9303bfe0f59dcba
George Guimares <george.guimaraes@outlook.com>
2024-07-26 21:58:48 -0500

    Added Wezterm configuration.

---
+++ .wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- If set to true, when there is only a single tab, the tab bar is hidden from the display
+config.hide_tab_bar_if_only_one_tab = true
+config.color_scheme = 'Tokyo Night'
+config.font_size = 11.0
+config.window_background_image = './Pictures/wallhaven.png'
+config.window_background_image_hsb = {
+  -- Darken the background image by reducing it to 1/3rd
+  brightness = 0.01,
+}
+-- and finally, return the configuration to wezterm
+return config
+
